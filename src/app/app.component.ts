41978ee1cf190f7a4f5b86f782016c0d6e4de02a
BRINZAS SEBASTIAN-VLAD <brinzassebastian-vlad@192-168-0-100.rdsnet.ro>
2023-04-21 11:24:43 +0300

    First setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'maps-frontend';
+  
+  map!: Leaflet.Map;
+  markers: Leaflet.Marker[] = [];
+  options = {
+    layers: [
+      Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      })
+    ],
+    zoom: 16,
+    center: { lat: 47.06365572118242, lng: 21.900861825903533 } , 
+  }
+
+  initMarkers() {
+    const initialMarkers = [
+      {
+        position: { lat: 47.06367783199533, lng: 21.903514266180373 },
+        draggable: true
+      },
+    ];
+    for (let index = 0; index < initialMarkers.length; index++) {
+      const data = initialMarkers[index];
+      const marker = this.generateMarker(data, index);
+      marker.addTo(this.map).bindPopup(`<b>Beerpong night</b>`);
+      //this.map.panTo(data.position);
+      this.markers.push(marker)
+    }
+  }
+
+  generateMarker(data: any, index: number) {
+    var greenIcon = Leaflet.icon({
+      iconUrl: './assets/party.png',
+  
+      iconSize:     [32, 32], // size of the icon
+  });
+
+    return Leaflet.marker(data.position, { draggable: data.draggable, icon: greenIcon})
+      .on('click', (event) => this.markerClicked(event, index))
+      .on('dragend', (event) => this.markerDragEnd(event, index));
+  }
+
+  onMapReady($event: Leaflet.Map) {
+    this.map = $event;
+    this.initMarkers();
+  }
+
+  mapClicked($event: any) {
+    console.log($event.latlng.lat, $event.latlng.lng);
+  }
+
+  markerClicked($event: any, index: number) {
+    console.log($event.latlng.lat, $event.latlng.lng);
+  }
+
+  markerDragEnd($event: any, index: number) {
+    console.log($event.target.getLatLng());
+  } 
 }
